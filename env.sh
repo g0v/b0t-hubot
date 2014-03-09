e0cf128c363d66642356c41cdc05e511e7b9e939
Youchen Lee <copyleft@utcr.org>
2014-03-09 20:29:54 +0800

    add github issue link support

--- env.sh
+++ env.sh
@@ -1,7 +1,13 @@
 export HUBOT_IRC_SERVER=irc.freenode.net
-export HUBOT_IRC_ROOMS="#g0v-b0t-test"
-export HUBOT_IRC_NICK="b0t"
+export HUBOT_IRC_ROOMS="#g0v-b0t-test,#g0v.tw"
+export HUBOT_IRC_NICK="b0t-issue"
 export HUBOT_IRC_UNFLOOD="true"
 #export HUBOT_IRC_DEBUG="true"
 export PORT=9999
+
+export HUBOT_GITHUB_REPO="g0v/dev"
+export HUBOT_GITHUB_TOKEN=""
+#export HUBOT_GITHUB_API
+export HUBOT_GITHUB_USER="g0v"
+export HUBOT_GITHUB_ISSUE_LINK_IGNORE_USERS="kcwu" # prevent bot-to-bot loop XD.. sorry to kcwu
 
