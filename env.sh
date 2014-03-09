a9fa327cd8071bb6da0ee597a71a1add35bbbafb
Youchen Lee <copyleft@utcr.org>
2014-03-09 20:34:05 +0800

    do not default join g0v.tw

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
 export HUBOT_IRC_SERVER=irc.freenode.net
-export HUBOT_IRC_ROOMS="#g0v-b0t-test,#g0v.tw"
+export HUBOT_IRC_ROOMS="#g0v-b0t-test"
 export HUBOT_IRC_NICK="b0t-issue"
 export HUBOT_IRC_UNFLOOD="true"
 #export HUBOT_IRC_DEBUG="true"
